49b214a4c87965d9c01e723034583ca2840b9eb2
Thomas Schmiereck <thomas@schmiereck.de>
2026-04-10 10:40:54 +0200

    Update joint_5 value in gripper group and add trajectory execution configuration

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -3,5 +3,9 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("bracket_arm", package_name="ba_arduino_controller_moveit_config").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("bracket_arm", package_name="ba_arduino_controller_moveit_config")
+        .trajectory_execution(file_path="config/trajectory_execution.yaml")
+        .to_moveit_configs()
+    )
     return generate_demo_launch(moveit_config)
